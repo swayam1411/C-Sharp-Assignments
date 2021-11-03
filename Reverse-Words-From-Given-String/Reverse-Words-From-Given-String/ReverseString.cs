ba9f06f61034e1097507c6f7ebda395622a11dc9
Swayam_1411 <87057029+swayam1411@users.noreply.github.com>
2021-11-03 17:14:33 +0530

    C-Sharp Program Reverse Words From Given String

---
+++ Reverse-Words-From-Given-String/Reverse-Words-From-Given-String/ReverseString.cs
@@ -0,0 +1,28 @@
+﻿  /********************   Assignment-> 1   *******************/
+ /* Q=>How to reverse the order of words in a given string? */
+/***********************************************************/
+
+using System;
+
+namespace Reverse_Words_From_Given_String
+{
+    class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter String => ");
+            string inputstring = Console.ReadLine();    //Taking String as input from user.
+
+            string[] split = inputstring.Split();  //separate words from string & stores in array.
+
+            Array.Reverse(split);   //Reverse Elements is Array spilt.
+
+            Console.Write("Output String=> ");
+            foreach (string printwords in split)
+            {
+                Console.Write(printwords + " ");
+            }   //end foreach
+            Console.ReadLine();
+        }   //end Main
+    }   //end class
+}   //end namespace
