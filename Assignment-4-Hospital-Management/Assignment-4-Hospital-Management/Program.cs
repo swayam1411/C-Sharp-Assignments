e175193eaf408cc32b8e57973605266ebf5c4ed0
Swayam_1411 <87057029+swayam1411@users.noreply.github.com>
2021-11-22 12:46:06 +0530

    Assignment-4 On Hospital Management

--- Assignment-4-Hospital-Management/Assignment-4-Hospital-Management/Program.cs
+++ Assignment-4-Hospital-Management/Assignment-4-Hospital-Management/Program.cs
@@ -1,69 +1,63 @@
 ﻿using System;
 using System.IO;
-
-namespace Assignment_4_Hospital_Management
+namespace HospitalManagement
 {
     class Program
     {
         public static void AddPatient()
         {
             Console.WriteLine("\nEnter Patient's Full Name => ");
-            string patientname = Console.ReadLine();
+            string patientName = Console.ReadLine();
             Console.WriteLine("Enter Symptoms => ");
             string symptoms = Console.ReadLine();
             Console.WriteLine("Enter Treatment => ");
             string treatment = Console.ReadLine();
             Console.WriteLine("Enter Doctor Name => ");
-            string doctor_name = Console.ReadLine();
-
+            string doctorName = Console.ReadLine();
             string date = DateTime.Now.ToString("dd-MM-yyyy");
-            string filepath = @"C:\Users\Computer\Desktop\GitHub\" + patientname + ".txt";
-            if(!File.Exists(filepath))
+            string filePath = @"C:\Users\Computer\Desktop\GitHub\" + patientName + ".txt";
+            if(!File.Exists(filePath))
             {
-                using (StreamWriter insertinfile = File.CreateText(filepath))
+                using (StreamWriter insertInFile = File.CreateText(filePath))
                 {
-                    insertinfile.WriteLine(patientname + "\t" + symptoms + "\t" + treatment + "\t" + doctor_name + "\t" + date);
+                    insertInFile.WriteLine(patientName + "\t" + symptoms + "\t" + treatment + "\t" + doctorName + "\t" + date);
                     Console.WriteLine("Patient's record added Sucessfully..!!\n");
                 }
             }
             else
             {
-                using (StreamWriter insertinfile = File.AppendText(filepath))
+                using (StreamWriter insertInFile = File.AppendText(filePath))
                 {
-                    insertinfile.WriteLine(patientname + "\t" + symptoms + "\t" + treatment + "\t" + doctor_name + "\t" + date);
+                    insertInFile.WriteLine(patientName + "\t" + symptoms + "\t" + treatment + "\t" + doctorName + "\t" + date);
                     Console.WriteLine("Patient's another record is added Sucessfully..!!\n");
                 }
             }
         }
-
         public static void DisplayPatient()
         {
-            Console.WriteLine("\nEnter Patient's Full Name=> ");
-            string patientname = Console.ReadLine();
-            string filepath = @"C:\Users\Computer\Desktop\GitHub\" + patientname + ".txt";
-            if (File.Exists(filepath))
+            Console.WriteLine("\nEnter Patient's Full Name=>");
+            string patientName = Console.ReadLine();
+            string filePath = @"C:\Users\Computer\Desktop\GitHub\" + patientName + ".txt";
+            if (File.Exists(filePath))
             {
                 Console.WriteLine("******************************************");
-                foreach (string readfromfile in File.ReadLines(filepath))
-                    Console.WriteLine(readfromfile);
+                foreach (string readFromFile in File.ReadLines(filePath))
+                    Console.WriteLine(readFromFile);
                 Console.WriteLine("******************************************");
             }
             else
                 Console.WriteLine($"Their is no Record For {patientname} Patient\n");
         }
- 
         static void Main(string[] args)
         {
             int choice;
             do
             {
-                Console.WriteLine("\n1: Add Patient Details");
-                Console.WriteLine("2: Display Patient Details");
-                Console.WriteLine("0: Exit");
-
-                Console.Write("Enter Your Choice => ");
+                Console.WriteLine("\n1:Add Patient Details");
+                Console.WriteLine("2:Display Patient Details");
+                Console.WriteLine("0:Exit");
+                Console.Write("Enter Your Choice =>");
                 choice = Convert.ToInt32(Console.ReadLine());
-                
                 switch (choice)
                 {
                     case 1:
